e067d2ed189a7cebcb90378ebb235e3d5d6a5e70
syniox <30317541+syniox@users.noreply.github.com>
2022-08-23 10:37:19 +0800

    fix: cloueflare cargo/trunk not found

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 VERSION=v0.16.0
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 wget -qO- https://github.com/thedodd/trunk/releases/download/${VERSION}/trunk-x86_64-unknown-linux-gnu.tar.gz | tar -xzf-
+source "$HOME/.cargo/env"
 cargo install --locked trunk
 trunk build
